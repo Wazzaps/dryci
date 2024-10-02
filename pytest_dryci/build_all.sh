34fbb880b44dd520e1716e4ae5e09c1a8de75c83
David Shlemayev <david.shlemayev@gmail.com>
2024-10-02 12:02:45 +0300

    Fix pypi publishing

--- pytest_dryci/build_all.sh
+++ pytest_dryci/build_all.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Building wheels for all platforms"
-for target in linux_x86_64 linux_aarch64 linux_loongarch64 linux_riscv64 macosx_10_12_x86_64 macosx_11_0_arm64; do
+# Linux-loongarch64 and Linux-riscv64 are not supported by pypi for some reason
+for target in Linux-x86_64 Linux-aarch64 Darwin-x86_64 Darwin-arm64; do
     TARGET=$target uv build --wheel
 done
--- pytest_dryci/hatch_build.py
+++ pytest_dryci/hatch_build.py
@@ -3,26 +3,25 @@ from typing import Any, Dict
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
-python_arch_to_uname = {
-    "linux_x86_64": "Linux-x86_64",
-    "linux_aarch64": "Linux-aarch64",
-    "linux_loongarch64": "Linux-loongarch64",
-    "linux_riscv64": "Linux-riscv64",
-    "macosx_10_12_x86_64": "Darwin-x86_64",
-    "macosx_11_0_arm64": "Darwin-arm64",
+uname_to_python_arch = {
+    "Linux-x86_64": "manylinux_2_17_x86_64.manylinux2014_x86_64.musllinux_1_1_x86_64",
+    "Linux-aarch64": "manylinux_2_17_aarch64.manylinux2014_aarch64.musllinux_1_1_aarch64",
+    "Linux-loongarch64": "linux_loongarch64",
+    "Linux-riscv64": "linux_riscv64",
+    "Darwin-x86_64": "macosx_10_12_x86_64",
+    "Darwin-arm64": "macosx_11_0_arm64",
 }
-uname_to_python_arch = {v: k for k, v in python_arch_to_uname.items()}
 
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: Dict[str, Any]) -> None:
         if "TARGET" in os.environ:
-            target = os.environ["TARGET"]
-            uname = python_arch_to_uname[target]
+            uname = os.environ["TARGET"]
+            pytarget = uname_to_python_arch[uname]
         else:
             uname = f"{os.uname().sysname}-{os.uname().machine}"
-            target = uname_to_python_arch[uname]
+            pytarget = uname_to_python_arch[uname]
 
-        build_data["tag"] = f"py3-none-{target}"
+        build_data["tag"] = f"py3-none-{pytarget}"
         repo_dagger_path = f"bin/repo_dagger-{uname}"
         build_data.setdefault("shared_scripts", {})[repo_dagger_path] = "repo_dagger"
